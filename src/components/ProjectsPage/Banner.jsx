69fba55ebf1d24f3675a47c851560c60aad71dd6
Nassim <achnassim34@gmail.com>
2024-10-04 01:15:11 +0200

    AOS lien github

--- src/components/ProjectsPage/Banner.jsx
+++ src/components/ProjectsPage/Banner.jsx
@@ -14,7 +14,8 @@ export default function Banner() {
                 </p>
                 <p data-aos="fade-left" className="BannerContainer__desc">Vous pouvez consulter ici la liste de mes
                     projets personnels ainsi que professionels publiquement disponibles, en sélectionnant un des projet disponible ci-dessous, une modale spécifiant la mission, la description, la stack du projet ainsi que des liens annexes sera disponible.</p>
-                <a href="https://github.com/FHJ1969" rel="noreferrer" target="_blank" className="GithubLink"><FontAwesomeIcon icon={faGithub} style={{marginRight: "5px"}}/>Profil Github</a>
+                <a data-aos="fade-right" href="https://github.com/FHJ1969" rel="noreferrer" target="_blank" className="GithubLink">
+                    <FontAwesomeIcon icon={faGithub} style={{marginRight: "5px"}}/>Profil Github</a>
             </div>
         </div>
     )
